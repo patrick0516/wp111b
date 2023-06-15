725500f1e6efc355d2b22a8bdef832bc483f4f62
Zhou Chen En <109636871+patrick0516@users.noreply.github.com>
2023-06-15 13:33:27 +0800

    Create isprime.js

---
+++ javascript程式設計練習/isprime.js
@@ -0,0 +1,13 @@
+function isPrime(num) {
+  if (num <= 0) {
+    console.log("請輸入數量。");
+  } else if (num === 1) {
+    console.log("成功。");
+  } else {
+    console.log(num);
+  }
+}
+
+isPrime(0);
+isPrime(4);
+isPrime(2);
